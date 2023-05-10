50c798eed3d155111d63fae92124d09521646658
phillychi3 <phillychi3@gmail.com>
2023-05-10 12:08:43 +0800

    edit some

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import argparse
 import json
 import threading
-import requests
 from ColorStr import parse
 from fake_useragent import UserAgent
 from requests_html import HTMLSession
@@ -20,69 +19,48 @@ def isfind(sitename,url):
 def nofind(sitename):
     print(parse(f'§w[§r✗§w] §r{sitename} : §yNOT FOUND'))
 
-def download(url):
+def gofind(url):
     ua = UserAgent()  
     user_agent = ua.random
     headers = {
                 'user_agent': user_agent,
             }
-    if data[url]["needjs"]:
-        
-        r = session.get(geturl(data[url]["userurl"],args.name),headers=headers)
-        r.html.render()
-        if data[url]["erroe_mod"] == "msg":
-            print(r.html.text.find(data[url]["error_msg"]))
-            if r.html.text.find(data[url]["error_msg"]) != -1:
-                nofind(data[url]["url"])
-                return
 
-        elif data[url]["erroe_mod"] == "stauts":
-            if not statuscode(r.status_code):
-                nofind(data[url]["url"])
-                return
+    r = session.get(geturl(data[url]["userurl"],args.name),headers=headers)
+    r.html.render()
+    if eval(geturl(data[url]['pair'],f'"{args.name}"')):
+        if "tureurl" in data[url]:
+            data[url]["userurl"] = data[url]["tureurl"]
+        isfind(data[url]["url"],geturl(data[url]["userurl"],args.name))
+        return
     else:
-        r= requests.get(geturl(data[url]["userurl"],args.name),headers=headers)
-        if data[url]["erroe_mod"] == "msg":
-            print(r.text.find(data[url]["error_msg"]))
-
-            if r.text.find(data[url]["error_msg"]) != -1:
-                nofind(data[url]["url"])
-                return
-
-        elif data[url]["erroe_mod"] == "stauts":
-            if not statuscode(r.status_code):
-                nofind(data[url]["url"])
-                return
-    isfind(data[url]["url"],geturl(data[url]["userurl"],args.name))
-    return
-
+        nofind(data[url]["url"])
+        return
 def main():
 
-    if args.name != None and args.image != None:
+    if args.name is not None and args.image is not None:
         exit("Error: You can only specify one of the following: --name or --image")
-    if args.name != None:
+    if args.name is not None:
         global final
         final = []
         sites = []
         
         for i in data:
             sites.append(i)
+        if args.target is not None:
+            if args.target in sites:
+                sites = []
+                sites.extend(args.target.split(","))
+            else:
+                exit("Error: The site is not in the list")
         print("Name: " + args.name)
         print(sites)
         for i in sites:
-            try:
-                data[i]["disable"]
-            except:
-                t = threading.Thread(target = download(i))
-                t.start()                
-                # download(i)
-    if args.image != None:
+            gofind(i)
+            # t = threading.Thread(target = gofind(i))
+            # t.start()
+    if args.image is not None:
         print("Image: " + args.image)
-
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -90,6 +68,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--name","-n", help="his name", )
     parser.add_argument("--image","-i", help="his image", )
+    parser.add_argument("--target","-t", help="site name", )
     args = parser.parse_args()
     with open('sites.json','r',encoding='utf-8') as f:
         data = json.load(f)
